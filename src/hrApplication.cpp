cf2ad6d73092d20c34f6a54c4a9e3db4332ffb9e
Admin <Admin@microsof-8a878f>
2009-03-19 17:58:29 +0500

    removed wrong code

--- src/hrApplication.cpp
+++ src/hrApplication.cpp
@@ -18,9 +18,6 @@
 #include "hrApplication.hpp"
 #include "hrPushButton.hpp"
 
-
 hrApplication::hrApplication(int argc, char **argv): QApplication(argc, argv)
 {
-    QFile f("lod:/blabla/blabla.txt");
-    f.open(QIODevice::ReadOnly);
 }
